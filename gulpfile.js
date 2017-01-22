7a706fd79e9ee4825f3c28ece5421af146ccebe3
Plou <cl.plou@gmail.com>
2017-01-22 02:06:54 +0100

    chores: add img folder to watch list

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-const gulp = require('gulp');
-const batch = require('gulp-batch');
-const watch = require('gulp-watch');
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
-const gutil = require('gulp-util');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const pug = require('gulp-pug');
-const sass = require('gulp-sass');
-const minifyCSS = require('gulp-csso');
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
-const browserify = require('browserify');
-const babelify = require('babelify');
-const es2015 = require('babel-preset-es2015');
-const uglify = require('gulp-uglify');
-const browserSync = require('browser-sync').create();
+const gulp = require('gulp')
+const batch = require('gulp-batch')
+const watch = require('gulp-watch')
+const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
+const gutil = require('gulp-util')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+const pug = require('gulp-pug')
+const sass = require('gulp-sass')
+const minifyCSS = require('gulp-csso')
+const sourcemaps = require('gulp-sourcemaps')
+const babel = require('gulp-babel')
+const browserify = require('browserify')
+const babelify = require('babelify')
+const es2015 = require('babel-preset-es2015')
+const uglify = require('gulp-uglify')
+const browserSync = require('browser-sync').create()
 
 
 
@@ -24,8 +24,8 @@ gulp.task('pug', function(){
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(pug())
     .pipe(gulp.dest('.'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 gulp.task('scss', function(){
   return gulp.src('css/src/**/*.scss')
@@ -35,8 +35,8 @@ gulp.task('scss', function(){
     .pipe(minifyCSS())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 gulp.task('js', () => {
   browserify('./js/src/main.js')
@@ -49,27 +49,21 @@ gulp.task('js', () => {
     .pipe(uglify())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('js'))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function () {
-  // Endless stream mode
-  watch('css/src/**/*.scss', ['sass']);
-  watch('js/src/**/*', ['js']);
-  watch('templates/**/*.pug', ['pug']);
-});
+    .pipe(browserSync.stream())
+})
 
 gulp.task('serve', function() {
 
     browserSync.init({
         server: "./",
         open: false,
-    });
+    })
 
-    gulp.watch("css/src/**/*.scss", ['scss']);
-    gulp.watch("js/src/**/*.js", ['js']);
-    gulp.watch("templates/**/*.pug", ['pug']);
-});
+    gulp.watch("css/src/**/*.scss", ['scss'])
+    gulp.watch("js/src/**/*.js", ['js'])
+    gulp.watch("templates/**/*.pug", ['pug'])
+    gulp.watch('img/**/*').on('change', browserSync.reload)
+})
 
-gulp.task('default', [ 'build', 'serve' ]);
-gulp.task('build', [ 'pug', 'scss', 'js' ]);
+gulp.task('default', [ 'build', 'serve' ])
+gulp.task('build', [ 'pug', 'scss', 'js' ])
--- js/src/main.js
+++ js/src/main.js
@@ -5,6 +5,8 @@
 
 import Tile from './components/Tile.js'
 import animationsUi from './components/tiles/animationsUi.js'
+import animationsIi from './components/tiles/animationsIi.js'
 
 
 new Tile('ui', animationsUi)
+new Tile('ii', animationsIi)
