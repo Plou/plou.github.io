03852d0d4b6bc3b964af65beff9acceae2dae190
Plou <clement@plou.io>
2018-06-24 14:07:21 +0200

    perf: optimise images

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1.1').then(function(cache) {
+    caches.open('v1.2').then(function(cache) {
       return cache.addAll([
         '/',
         '/manifest.json',
