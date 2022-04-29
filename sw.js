109e14aac7784f1a533381d9ccb4c13d20aeae0b
Plou <clement@plou.io>
2022-04-29 12:44:25 +0200

    feat: Update identity   - Improve manifest

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1.9').then(function(cache) {
+    caches.open('v2.0').then(function(cache) {
       return cache.addAll([
         '/',
         '/manifest.json',
